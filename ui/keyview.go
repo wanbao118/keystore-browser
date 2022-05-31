427a806d32cc3c71de4f7eefee5b495a1528d02a
Wanbao Jiang <wanbao118@hotmail.com>
2022-05-31 11:13:40 +0800

    add key view

--- ui/keyview.go
+++ ui/keyview.go
@@ -1,4 +1,29 @@
 package ui
 
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/widget"
+)
+
 type KeyView struct {
+	widget.BaseWidget
+	filenpath string
+	button    widget.Button
+	view      widget.Entry
+}
+
+func (c *KeyView) Layout(size fyne.Size) {
+
+}
+
+func (c *KeyView) MinSize() {
+
+}
+
+func (c *KeyView) Refresh() {
+
+}
+
+func (c *KeyView) CreateRenderer() fyne.WidgetRenderer {
+	return nil
 }
